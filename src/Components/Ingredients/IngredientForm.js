b7b6f10e3681aef32559fe2cd30a77bb3401bf58
Inbal Schkolnik <capoeira0012>
2021-06-22 22:45:19 +0300

    Create IngredientForm & Add submodule common

---
+++ src/Components/Ingredients/IngredientForm.js
@@ -0,0 +1,172 @@
+import React from 'react';
+import {
+  Button,
+  makeStyles,
+  TextField,
+  FormControl,
+  Select,
+  MenuItem,
+} from '@material-ui/core';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Divider from '@material-ui/core/Divider';
+import EInventoryCategory from '../../common/src/Enums/EInventoryCategory';
+import { Bottle } from '../../common/src/Model/InventoryModel/Bottle';
+import BooleanInventoryItem from '../../common/src/Model/InventoryModel/BooleanInventoryItem';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+  textField: {
+    width: 'flex',
+    marginLeft: 15,
+    marginRight: 15,
+    background: theme.palette.background.paper,
+  },
+  divider: {
+    marginBottom: 15,
+    marginTop: 15,
+  },
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2),
+  },
+}));
+
+let categories = {};
+Object.entries(EInventoryCategory.BottleCategory).forEach(([key, value]) => {
+  if (key !== 'AlcoholCategory')
+    categories = { ...categories, [key]: { id: key, name: value } };
+});
+Object.entries(EInventoryCategory.BottleCategory.AlcoholCategory).forEach(
+  ([key, value]) => {
+    categories = { ...categories, [key]: { id: key, name: value } };
+  }
+);
+Object.entries(EInventoryCategory).forEach(([key, value]) => {
+  if (key === 'BottleCategory' || key === 'Unsorted' || key === 'Unavailable') {
+  } else categories = { ...categories, [key]: { id: key, name: value } };
+});
+
+function IngredientForm(props) {
+  const classes = useStyles();
+
+  let alcoholPercentageSection = Bottle.isAAlcoholCategory(props.category) ? (
+    <React.Fragment>
+      <FormGroup>
+        <Divider className={classes.divider} />
+        <label>Alcohol Percentage: </label>
+        <TextField
+          required
+          id='alcoholPercentage'
+          type='number'
+          className={classes.textField}
+          placeholder='Alcohol Percentage'
+          value={props.alcoholPercentage}
+          onChange={props.onChangeAlcoholPercentage}
+        />
+      </FormGroup>
+    </React.Fragment>
+  ) : (
+    ''
+  );
+
+  let remainingSection = BooleanInventoryItem.isABooleanCategory(
+    props.category
+  ) ? (
+    <FormControlLabel
+      control={
+        <Checkbox
+          name='checkedB'
+          color='primary'
+          checked={props.IsAvailable}
+          onChange={props.onChangeIsAvailable}
+        />
+      }
+      label='Is Available'
+    />
+  ) : (
+    <React.Fragment>
+      <FormGroup>
+        <label>Amount: </label>
+        <TextField
+          id='Amount'
+          type='number'
+          className={classes.textField}
+          placeholder='Amount'
+          value={props.remaining}
+          onChange={props.onChangeRemaining}
+        />
+      </FormGroup>
+      <Divider className={classes.divider} />
+      <FormGroup>
+        <label>Minimum Required</label>
+
+        <TextField
+          id='minRequired'
+          placeholder='Minimum Required'
+          type='number'
+          className={classes.textField}
+          value={props.minRequired}
+          onChange={props.onChangeMinRequired}
+        />
+      </FormGroup>
+    </React.Fragment>
+  );
+  return (
+    <form onSubmit={props.onSubmit} className={classes.root}>
+      <FormGroup>
+        <label>Name: </label>
+        <TextField
+          required
+          id='Name'
+          type='text'
+          className={classes.textField}
+          placeholder='Ingredient Name'
+          value={props.name}
+          onChange={props.onChangeName}
+        />
+      </FormGroup>
+      <Divider className={classes.divider} />
+      <FormGroup>
+        <label>Category: </label>
+        <FormControl className={classes.formControl}>
+          <Select
+            id='demo-simple-select'
+            value={props.category}
+            onChange={props.onChangeCategory}
+          >
+            {Object.keys(categories).map((key, value) => {
+              return (
+                <MenuItem key={key} value={key}>
+                  {categories[key].name}
+                </MenuItem>
+              );
+            })}
+          </Select>
+        </FormControl>
+      </FormGroup>
+      {alcoholPercentageSection}
+      <Divider className={classes.divider} />
+      {remainingSection}
+      <Divider className={classes.divider} />
+      <FormGroup>
+        <Button variant='contained' onClick={props.onSubmit} color='primary'>
+          {props.submitTitle}
+        </Button>
+      </FormGroup>
+    </form>
+  );
+}
+
+export default IngredientForm;
